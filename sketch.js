2227646bc1bb17abe7288d793ab20f5f96d7f728
DhairyaTheGoat <131680259+DhairyaTheGoat@users.noreply.github.com>
2023-06-12 22:16:33 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var groundObj
+var trash
+var leftSide
+var radius = 40
+
+
+function preload(){
+
+	
+}
+
+function setup() {
+	createCanvas(1200, 900);
+
+	var trash_options ={
+		isStaic:false,
+		friction:0,
+		restitution:0.3,
+		density:1.2
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+groundObj = new Ground(width/2,670,width,20)
+leftSide = new Ground(1100,600,20,120)
+rightSide = new Ground(900,600,20,120)
+trash = Matter.Bodies.circle(9,655,radius/2,trash_options)
+
+Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj.display()
+  leftSide.display()
+  rightSide.display()
+  ellipse(trash.position.x,trash.position.y,radius,radius)
+}
+
+function keyPressed() {
+	if (keyCode === DOWN_ARROW){
+
+	 Matter.Body.applyForce(trash,trash.position,{x:85,y:-85})
+
+	}
+}
